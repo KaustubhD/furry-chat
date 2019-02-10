a7af7615f165f8b57e359c2bbfa5ea60af5d447f
KaustubhD <kaustubhd287@gmail.com>
2019-02-10 23:41:45 +0530

    ❌ inline styles ❌

--- client/client.js
+++ client/client.js
@@ -1,7 +1,7 @@
 window.WebSocket = window.WebSocket || window.MozWebSocket
 
-// let conn = new WebSocket('ws://127.0.0.1:6900')
-let conn = new WebSocket('ws://young-lowlands-88811.herokuapp.com/')
+let conn = new WebSocket('ws://127.0.0.1:6900')
+// let conn = new WebSocket('ws://young-lowlands-88811.herokuapp.com/')
 // lett conn = new WebSocket('')
 
 let contentDiv = document.getElementById('content')
