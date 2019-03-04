59d3fb4d2a5326d0fb73cb705aeb85df39e076e7
KaustubhD <kaustubhd287@gmail.com>
2019-03-04 17:51:53 +0530

    Temp

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,15 @@
 
 STATUS="$(git status)"
 
-if [[ $STATUS == *"nothing to commit, working directory clean"* ]]
+if [[ $STATUS == *"nothing to commit, working tree clean"* ]]
 then
-    sed -i "" 'dist/' ./.gitignore
-    git add .
+    sed -i "" 'dist/d' ./.gitignore
+    git add .gitignore
     git commit -m "Edit .gitignore to publish"
     git push origin `git subtree split --prefix dist master`:gh-pages --force
     git reset HEAD~
-    git checkout .gitignore
+    git add .
+    git commit -m "I don't know commit"
 else
     echo "Need clean working directory to publish"
 fi
